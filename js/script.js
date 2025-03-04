175485c58b7c227262dcc8b0f31e687474a15c4c
its.john <giovannialesi87@icloud.com>
2025-03-04 17:31:59 +0100

    step 2

--- js/script.js
+++ js/script.js
@@ -17,15 +17,50 @@ Vi consigliamo di seguire questo ordine nella risoluzione dell'esercizio:
 4. Stampare il risultato in pagina.
 */
 
-const userform = document.getElementById("user-form");
-console.log(userform);
 
-const usernameSurname = document.getElementById("username surname");
-console.log(usernameSurname);
-const km = document.getElementById("km");
-console.log("km");
-const ageRange = document.getElementById("Age range")
-console.log("ageRange");
+
+// const generateButton = document.getElementById("generate-btn");
+// console.log(generateButton);
+// const cancelButton = document.getElementById("cancel-btn");
+// console.log(cancelButton);
+const ticketForm = document.getElementById("ticket-form");
+console.log(ticketForm);
+
+const nameSurnameInput = document.getElementById("nameSurname");
+console.log("nameSurnameInput");
+const kmToTravelInput = document.getElementById("kmToTravel");
+console.log("kmInput");
+// const ageRangeOption = document.getElementById("AgeRange");
+// console.log("ageRangeSelect");
+
+// generateButton.addEventListener("click", function(){
+//     console.log("dentro click listener");
+//     const nameSurname = nameSurnameInput.value;
+//     console.log(nameSurname); 
+//     const kmToTravel = kmToTravelInput.value;
+//     console.log(kmToTravel);
+//     const ageRange = ageRangeOption.value;
+//     console.log(ageRange);
+// })
+
+
+// menuButton.addEventListener("click", function ()) {
+//     console.log("click");
+
+// }
+
+
+
+// ticketform.addEventListener("submit", handleticketform);
+
+function handleticketform(event){
+    event.preventDefault();
+    console.log("ticketform");
+    const nameSurname = nameSurnameInput.ariaValueMax;
+    const km = kmInput.ariaValueMax;
+    const data = `${nameSurname} ${km}`;
+    console.log(data);
+}
 
 
 
